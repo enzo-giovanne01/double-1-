2a1394f67424a548f8dfa26c6ae6d03cd3498978
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-02 00:16:42 -0300

    double-1-

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+double n1, n2;
+
+WriteLine("Digite o primeiro numero:");
+n1 = Convert.ToInt32(ReadLine());
+WriteLine("Digite o segundo numero:");
+n2 = Convert.ToInt32(ReadLine());
+
+
+if (n2 > n1)
+{
+    WriteLine($"{n2} é maior que {n1}");
+}
+else{
+    WriteLine($"{n1} é maior que {n2}");
+}
+
